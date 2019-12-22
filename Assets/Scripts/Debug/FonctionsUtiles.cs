93c4cbeacb46c8ba8deff31f4bcea49ae98ae85e
grosboisma <grosboisma@eisti.eu>
2019-12-22 18:37:50 +0100

    meleeEnnemies follow and stop follow

---
+++ Assets/Scripts/Debug/FonctionsUtiles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FonctionsUtiles : MonoBehaviour
+{
+    public static void DebugRay(Vector3 origin, Vector3 destination, Color c)
+    {
+        Vector3 direction = destination - origin;
+        Debug.DrawRay(origin, direction, c);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Ennemies/MeleeEnnemyBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeleeEnnemyBehaviour : MonoBehaviour
+{
+    private GameObject target;
+    private NavMeshAgent agent;
+    public float range = 3.0f;
+    public float chaseDistance = 15.0f;
+
+    private void HeadForDestination()
+    {
+        Vector3 destination = target.transform.position;
+        agent.SetDestination(destination);
+        FonctionsUtiles.DebugRay(transform.position, destination, Color.yellow);
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack from " + transform.name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("CharacterContainer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float targetDistance = Vector3.Distance(transform.position, target.transform.position);
+        if (targetDistance < chaseDistance)
+        {
+            Debug.Log("chase");
+            if (targetDistance > range)
+            {
+                HeadForDestination();
+            }
+            else
+            {
+                Attack();
+            }
+        }
+        else
+        {
+            Debug.Log("Stop chase");
+            agent.SetDestination(transform.position);
+        }
+    }
+}
